7a4a4967c63312f7f66783f73ff3e9ebcc8429fe
Libing Chen <libing.chen@gmail.com>
2025-07-23 11:26:51 -0700

    feat: ad private/public key update

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,8 +1,9 @@
+use colored::Colorize;
 use ecies::utils::generate_keypair;
 use ecies::{PublicKey, SecretKey};
 use std::collections::HashMap;
-use std::env;
 use std::path::{Path, PathBuf};
+use std::{env, fs};
 
 pub mod decrypt;
 pub mod encrypt;
@@ -109,6 +110,132 @@ pub fn get_public_key(profile_name: &Option<String>) -> Result<String, Box<dyn s
     Err("Public key not found".into())
 }
 
+pub fn write_public_key_to_file<P: AsRef<Path>>(
+    env_file: P,
+    public_key: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let header_text = format!(
+        r#"
+#/-------------------[DOTENV_PUBLIC_KEY]--------------------/
+#/            public-key encryption for .env files          /
+#/       [how it works](https://dotenvx.com/encryption)     /
+#/----------------------------------------------------------/
+DOTENV_PUBLIC_KEY="{}"
+"#,
+        public_key
+    );
+    let file_name = env_file.as_ref().file_name().unwrap().to_str().unwrap();
+    // file does not exist, and we create it
+    if !env_file.as_ref().exists() {
+        fs::write(&env_file, header_text.as_bytes())?;
+        println!(
+            "{}",
+            format!("✔ {} file created with the public key", file_name).green()
+        );
+        return Ok(());
+    } else {
+        let env_file_content = fs::read_to_string(&env_file).unwrap();
+        if !env_file_content.contains("DOTENV_PUBLIC_KEY") {
+            let file_content = format!("{}\n{}", header_text.trim(), env_file_content);
+            fs::write(&env_file, file_content.as_bytes())?;
+            println!("{}", format!("✔ public key added in {}", file_name).green());
+        } else if !env_file_content.contains(public_key) {
+            // update existing public key
+            let mut new_content = String::new();
+            for line in env_file_content.lines() {
+                if line.starts_with("DOTENV_PUBLIC_KEY") {
+                    new_content.push_str(&format!("DOTENV_PUBLIC_KEY=\"{}\"\n", public_key));
+                } else {
+                    new_content.push_str(line);
+                    new_content.push('\n');
+                }
+            }
+            fs::write(&env_file, new_content.as_bytes())?;
+            println!(
+                "{}",
+                format!("✔ public key updated in {}", file_name).green()
+            );
+        } else {
+            println!(
+                "{}",
+                format!("✔ public key already exists in {}", file_name).green()
+            );
+        }
+    }
+    Ok(())
+}
+
+pub fn write_private_key_to_file<P: AsRef<Path>>(
+    env_keys_file: P,
+    private_key_name: &str,
+    private_key_value: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let file_name = env_keys_file
+        .as_ref()
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    // file does not exist, and we create it
+    if !env_keys_file.as_ref().exists() {
+        let file_content = format!(
+            r#"
+#/-------------------[DOTENV_PUBLIC_KEY]--------------------/
+#/            public-key encryption for .env files          /
+#/       [how it works](https://dotenvx.com/encryption)     /
+#/----------------------------------------------------------/
+
+{}="{}"
+"#,
+            private_key_name, private_key_value
+        );
+        fs::write(&env_keys_file, file_content.as_bytes())?;
+        println!(
+            "{}",
+            format!("✔ {} file created with the private key", file_name).green()
+        );
+    } else {
+        let env_keys_content = fs::read_to_string(&env_keys_file).unwrap();
+        // no key in the file, we add it
+        if !env_keys_content.contains(private_key_name) {
+            let new_content = format!(
+                "{}\n{}=\"{}\"\n",
+                env_keys_content.trim(),
+                private_key_name,
+                private_key_value
+            );
+            fs::write(&env_keys_file, new_content.as_bytes())?;
+            println!(
+                "{}",
+                format!("✔ private key added in {}", file_name).green()
+            );
+        } else if !env_keys_content.contains(private_key_value) {
+            // update existing private key
+            let mut new_content = String::new();
+            for line in env_keys_content.lines() {
+                if line.starts_with(&format!("{}=", private_key_name)) {
+                    new_content
+                        .push_str(&format!("{}=\"{}\"\n", private_key_name, private_key_value));
+                } else {
+                    new_content.push_str(line);
+                    new_content.push('\n');
+                }
+            }
+            fs::write(&env_keys_file, new_content.as_bytes())?;
+            println!(
+                "{}",
+                format!("✔ private key updated in {}", file_name).green()
+            );
+        } else {
+            println!(
+                "{}",
+                format!("✔ private key already exists in {}", file_name).green()
+            );
+        }
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -125,5 +252,18 @@ mod tests {
         let profile_name = Some("example".to_owned());
         let public_key = get_public_key(&profile_name);
         println!("public key: {}", public_key.unwrap());
+    }
+
+    #[test]
+    fn test_write_public_key() {
+        let public_key = "xxxx";
+        let env_file = PathBuf::from(".env.test");
+        write_public_key_to_file(&env_file, public_key).unwrap();
+    }
+
+    #[test]
+    fn test_write_private_key() {
+        let env_file = PathBuf::from(".env.keys");
+        write_private_key_to_file(&env_file, "DOTENV_PRIVATE_KEY_TEST", "xxxx").unwrap();
     }
 }
