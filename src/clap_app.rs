65e7f01b4c6874b10d05a384765af4311a107cf1
Libing Chen <libing.chen@gmail.com>
2025-07-23 13:39:28 -0700

    chore: add env-file and format for get commad

--- src/clap_app.rs
+++ src/clap_app.rs
@@ -20,7 +20,24 @@ pub fn build_dotenvx_app() -> Command {
                 .help("override existing env variables (by default, existing env vars take precedence over .env files)")
                 .action(ArgAction::SetTrue)
         );
-    let get_command = Command::new("get").about("return a single environment variable");
+    let get_command = Command::new("get")
+        .about("return a single environment variable")
+        .arg(
+            Arg::new("env-file")
+                .short('f')
+                .long("env-file")
+                .help("path to your env file (default: .env)")
+                .num_args(1)
+                .required(false),
+        )
+        .arg(Arg::new("key").help("key's name").index(1).required(false))
+        .arg(
+            Arg::new("format")
+                .long("format")
+                .help("format of the output (json, shell) (default: \"json\")")
+                .num_args(1)
+                .required(false),
+        );
     let set_command = Command::new("set")
         .about("set a single environment variable")
         .arg(Arg::new("key").help("key's name").index(1).required(true))
