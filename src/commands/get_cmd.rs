fa032250f851d0cc732754a7bf670efe78843575
Libing Chen <libing.chen@gmail.com>
2025-07-23 13:38:38 -0700

    app: get sub command

---
+++ src/commands/get_cmd.rs
@@ -0,0 +1,59 @@
+use crate::commands::decrypt::{decrypt_env_entries, decrypt_env_item};
+use crate::commands::{get_env_file_arg, get_private_key, read_dotenv_file, wrap_value};
+use clap::ArgMatches;
+use colored_json::{to_colored_json_auto, ToColoredJson};
+use dotenvx_rs::common::get_profile_name_from_file;
+
+pub fn get_command(command_matches: &ArgMatches) {
+    let key_arg = command_matches.get_one::<String>("key").map(|s| s.as_str());
+    let env_file = get_env_file_arg(command_matches);
+    let format = if let Some(arg_value) = command_matches.get_one::<String>("format") {
+        arg_value.clone()
+    } else {
+        "json".to_owned()
+    };
+    // if a key is provided, we read the .env file and print the value of the key
+    if let Some(key_name) = key_arg {
+        let key_name = &key_name.to_uppercase();
+        if let Ok(entries) = read_dotenv_file(&env_file) {
+            if let Some(value) = entries.get(key_name) {
+                let plain_value = if value.starts_with("encrypted:") {
+                    let profile_name = get_profile_name_from_file(&env_file);
+                    let private_key = get_private_key(&profile_name).unwrap();
+                    decrypt_env_item(&private_key, value).unwrap_or(value.clone())
+                } else {
+                    value.clone()
+                };
+                if format == "shell" {
+                    println!("{}={}", key_name, plain_value);
+                } else {
+                    let body = serde_json::json!({key_name: plain_value});
+                    println!("{}", to_colored_json_auto(&body).unwrap());
+                }
+            } else {
+                eprintln!("Key '{}' not found in {}", key_name, env_file);
+            }
+        } else {
+            eprintln!("Failed to read the .env file: {}", env_file);
+        }
+    } else {
+        // print all entries with json format
+        if let Ok(entries) = decrypt_env_entries(&env_file) {
+            if format == "shell" {
+                for (key, value) in &entries {
+                    println!("{}={}", key, wrap_value(value));
+                }
+            } else {
+                println!(
+                    "{}",
+                    serde_json::to_string(&entries)
+                        .unwrap()
+                        .to_colored_json_auto()
+                        .unwrap()
+                );
+            }
+        } else {
+            eprintln!("Failed to decrypt the .env file: {}", env_file);
+        }
+    }
+}
