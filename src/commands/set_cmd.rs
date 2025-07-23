bd6dd6f3b33c4e9593e324ce5e6c27f302eb1941
Libing Chen <libing.chen@gmail.com>
2025-07-23 14:11:39 -0700

    app: set sub commannd

---
+++ src/commands/set_cmd.rs
@@ -0,0 +1,80 @@
+use crate::commands::encrypt::encrypt_env_item;
+use crate::commands::{get_env_file_arg, get_public_key, wrap_value};
+use clap::ArgMatches;
+use dotenvx_rs::common::get_profile_name_from_file;
+use std::fs;
+use std::path::Path;
+
+pub fn set_command(command_matches: &ArgMatches) {
+    let key_arg = command_matches.get_one::<String>("key").map(|s| s.as_str());
+    let value_arg = command_matches
+        .get_one::<String>("value")
+        .map(|s| s.as_str());
+    if key_arg.is_none() || value_arg.is_none() {
+        eprintln!("Both key and value arguments are required.");
+        return;
+    }
+    let env_file = get_env_file_arg(command_matches);
+    let key = key_arg.unwrap().to_uppercase();
+    let value = value_arg.unwrap();
+    let env_file_exists = Path::new(&env_file).exists();
+    let mut encrypt_mode = true;
+    let mut env_file_content = String::new();
+    if !env_file_exists {
+        encrypt_mode = true;
+    } else {
+        if let Ok(file_content) = fs::read_to_string(&env_file) {
+            env_file_content = file_content;
+        }
+        encrypt_mode = env_file_content.contains("=encrypted:");
+    }
+    let profile_name = get_profile_name_from_file(&env_file);
+    let public_key = get_public_key(&profile_name).unwrap();
+    let pair = if encrypt_mode {
+        let encrypted_value = encrypt_env_item(&public_key, value).unwrap();
+        format!("{}={}", key, encrypted_value)
+    } else {
+        format!("{}={}", key, wrap_value(value))
+    };
+    if !env_file_exists {
+        let new_content = format!(
+            r#"
+#/-------------------[DOTENV_PUBLIC_KEY]--------------------/
+#/            public-key encryption for .env files          /
+#/       [how it works](https://dotenvx.com/encryption)     /
+#/----------------------------------------------------------/
+DOTENV_PUBLIC_KEY="{}"
+{}
+"#,
+            &public_key, pair
+        );
+        fs::write(&env_file, new_content).expect("Failed to write to the .env file");
+        println!("Added {} to {}", key, env_file);
+    } else {
+        if env_file_content.contains(&format!("{}=", key)) {
+            // Update existing key
+            let new_content = env_file_content
+                .lines()
+                .map(|line| {
+                    if line.starts_with(&key) {
+                        pair.clone()
+                    } else {
+                        line.to_string()
+                    }
+                })
+                .collect::<Vec<String>>()
+                .join("\n");
+            fs::write(&env_file, new_content).expect("Failed to write to the .env file");
+            println!("Updated {} in {}", key, env_file);
+        } else {
+            // Add new key
+            let mut new_content = env_file_content;
+            if !new_content.is_empty() && !new_content.ends_with('\n') {
+                new_content.push('\n');
+            }
+            new_content.push_str(&pair);
+            fs::write(&env_file, new_content).expect("Failed to write to the .env file");
+            println!("Added {} to {}", key, env_file);
+        }
+    }
+}
