e17b8c90a8af34ff744927223830118aea12c916
Libing Chen <libing.chen@gmail.com>
2025-07-23 14:13:07 -0700

    chore: add ls, get, set sub commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use crate::clap_app::build_dotenvx_app;
 use crate::commands::decrypt::decrypt_command;
 use crate::commands::encrypt::encrypt_command;
+use crate::commands::get_cmd::get_command;
 use crate::commands::keypair::keypair_command;
+use crate::commands::list::ls_command;
 use crate::commands::run::run_command;
+use crate::commands::set_cmd::set_command;
 use std::env;
 
 mod clap_app;
@@ -28,6 +31,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "encrypt" => encrypt_command(command_matches),
             "decrypt" => decrypt_command(command_matches),
             "keypair" => keypair_command(command_matches),
+            "ls" => ls_command(command_matches),
+            "get" => get_command(command_matches),
+            "set" => set_command(command_matches),
             &_ => println!("Unknown command"),
         }
     }
