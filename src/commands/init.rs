71f64a9d18576c7fced6307787a8009186009f7d
Libing Chen <libing.chen@gmail.com>
2025-07-23 17:05:14 -0700

    chore: add init command

---
+++ src/commands/init.rs
@@ -0,0 +1,26 @@
+use crate::commands::{
+    create_env_file, get_env_file_arg, get_private_key_name_for_file, write_private_key_to_file, EcKeyPair,
+    KEYS_FILE_NAME,
+};
+use clap::ArgMatches;
+use colored::Colorize;
+use std::path::Path;
+
+pub fn init_command(command_matches: &ArgMatches) {
+    let env_file = get_env_file_arg(command_matches);
+    let env_file_exists = Path::new(&env_file).exists();
+    if env_file_exists {
+        eprintln!("The .env file already exists: {}", env_file);
+        return;
+    }
+    let kp = EcKeyPair::generate();
+    let public_key = kp.get_pk_hex();
+    let pair = format!("{}={}", "KEY1", "value1");
+    create_env_file(&env_file, &public_key, Some(&pair));
+    println!(
+        "{}",
+        format!("Initialized new .env file with name: {}", env_file).green()
+    );
+    let private_key_name = get_private_key_name_for_file(&env_file);
+    write_private_key_to_file(KEYS_FILE_NAME, &private_key_name, &kp.get_sk_hex()).unwrap();
+}
