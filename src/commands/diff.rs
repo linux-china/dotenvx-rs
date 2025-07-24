83ef462c2db933f3ea65367075252b5a2fc0b86e
Libing Chen <libing.chen@gmail.com>
2025-07-24 15:26:34 -0700

    chore: add diff for keys

--- src/commands/diff.rs
+++ src/commands/diff.rs
@@ -2,13 +2,14 @@ use crate::commands::decrypt::decrypt_env_item;
 use crate::commands::{get_private_key_for_file, read_dotenv_file};
 use clap::ArgMatches;
 use dotenvx_rs::common::get_profile_name_from_file;
-use prettytable::{row, Table};
+use prettytable::{row, Cell, Table};
 
 pub fn diff_command(command_matches: &ArgMatches) {
-    let key_name = command_matches
-        .get_one::<String>("key")
+    let key_names = command_matches
+        .get_one::<String>("keys")
         .unwrap()
         .to_uppercase();
+    let key_names = key_names.split(',').collect::<Vec<&str>>();
     let current_dir = std::env::current_dir().unwrap();
     let entries = walkdir::WalkDir::new(&current_dir)
         .max_depth(1)
@@ -32,22 +33,31 @@ pub fn diff_command(command_matches: &ArgMatches) {
         return;
     }
     let mut table = Table::new();
-    table.add_row(row!["Profile", key_name]);
+    let mut header_row = row!["Profile"];
+    for key_name in &key_names {
+        header_row.add_cell(Cell::new(key_name));
+    }
+    table.add_row(header_row);
     for entry in entries {
         let env_file_name = entry.file_name().to_str().unwrap();
         let entries = read_dotenv_file(env_file_name).unwrap();
         let profile_name = get_profile_name_from_file(env_file_name).unwrap_or("".to_string());
-        if let Some(value) = entries.get(&key_name) {
-            let mut plain_value = value.to_string();
-            if value.starts_with("encrypted:") {
-                if let Ok(private_key) = get_private_key_for_file(env_file_name) {
-                    plain_value = decrypt_env_item(&private_key, value).unwrap_or(value.clone());
+        let mut data_row = row![profile_name];
+        for key_name in &key_names {
+            if let Some(value) = entries.get(*key_name) {
+                let mut plain_value = value.to_string();
+                if value.starts_with("encrypted:") {
+                    if let Ok(private_key) = get_private_key_for_file(env_file_name) {
+                        plain_value =
+                            decrypt_env_item(&private_key, value).unwrap_or(value.clone());
+                    }
                 }
+                data_row.add_cell(Cell::new(&plain_value));
+            } else {
+                data_row.add_cell(Cell::new(""));
             }
-            table.add_row(row![profile_name, plain_value]);
-        } else {
-            table.add_row(row![profile_name, ""]);
         }
+        table.add_row(data_row);
     }
     table.printstd();
 }
