9c379ce4446f49db61a1ddfb8f778709976cb4a9
Libing Chen <libing.chen@gmail.com>
2025-07-24 14:14:14 -0700

    chore: add keypair validation

--- src/commands/keypair.rs
+++ src/commands/keypair.rs
@@ -1,8 +1,9 @@
 use crate::commands::{
     get_env_file_arg, get_private_key, get_private_key_name, get_public_key, get_public_key_name,
-    KEYS_FILE_NAME,
+    EcKeyPair, KEYS_FILE_NAME,
 };
 use clap::ArgMatches;
+use colored::Colorize;
 use colored_json::to_colored_json_auto;
 use dotenvx_rs::common::get_profile_name_from_file;
 use std::env;
@@ -31,6 +32,19 @@ pub fn keypair_command(command_matches: &ArgMatches, profile: &Option<String>) {
     } else {
         let private_key = get_private_key(&profile_name);
         let public_key = get_public_key(&profile_name);
+        // check key pair validity
+        if private_key.is_ok() && public_key.is_ok() {
+            let public_key_hex = public_key.as_ref().unwrap();
+            let private_key_hex = private_key.as_ref().unwrap();
+            let kp = EcKeyPair::from_secret_key(private_key_hex);
+            let reversed_pk_hex = kp.get_pk_hex();
+            if &reversed_pk_hex != public_key_hex {
+                eprintln!("{}", "The public key does not match the private key:".red());
+                eprintln!("{}={}", env_pub_key_name, public_key_hex);
+                eprintln!("{}={}", env_private_key_name, private_key_hex);
+                std::process::exit(1);
+            }
+        }
         if format == "shell" {
             println!(
                 "export {}={}",
