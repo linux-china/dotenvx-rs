b78d6896d1e89ea211befac0bcbc303cbb3adf87
Libing Chen <libing.chen@gmail.com>
2025-07-25 13:57:59 -0700

    chore: lint polishing

--- tests/dotenvx_test.rs
+++ tests/dotenvx_test.rs
@@ -5,7 +5,7 @@ fn test_dotenv_load() {
     dotenvx_rs::dotenv().ok();
     // Check if the environment variable is set
     let value = env::var("HELLO").unwrap();
-    println!("HELLO={}", value);
+    println!("HELLO={value}");
 }
 
 #[test]
@@ -14,5 +14,5 @@ fn test_dotenv_load_example() {
     dotenvx_rs::from_path(".env.example").ok();
     // Check if the environment variable is set
     let value = env::var("HELLO").unwrap();
-    println!("HELLO={}", value);
+    println!("HELLO={value}");
 }
