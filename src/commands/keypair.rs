e43b96517cc2eadbeec683837278b1e5a77fa554
Libing Chen <libing.chen@gmail.com>
2025-07-25 16:09:52 -0700

    core: add dump support

--- src/commands/keypair.rs
+++ src/commands/keypair.rs
@@ -1,6 +1,6 @@
 use crate::commands::{
     get_env_file_arg, get_private_key, get_private_key_name, get_public_key, get_public_key_name,
-    EcKeyPair, KEYS_FILE_NAME,
+    write_private_key_to_file, write_public_key_to_file, EcKeyPair, KEYS_FILE_NAME,
 };
 use clap::ArgMatches;
 use colored::Colorize;
@@ -44,6 +44,13 @@ pub fn keypair_command(command_matches: &ArgMatches, profile: &Option<String>) {
                 eprintln!("{env_private_key_name}={private_key_hex}");
                 std::process::exit(1);
             }
+        }
+        // dump the public key to .env file and private key to .env.keys file
+        if command_matches.get_flag("dump") {
+            write_public_key_to_file(&env_file, &public_key.unwrap()).unwrap();
+            write_private_key_to_file(KEYS_FILE_NAME, &env_private_key_name, &private_key.unwrap())
+                .unwrap();
+            return;
         }
         if format == "shell" {
             println!(
