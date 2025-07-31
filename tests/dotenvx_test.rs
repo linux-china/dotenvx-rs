e020fa6951de9f85b669f8ba6c8c147b291f14df
Libing Chen <libing.chen@gmail.com>
2025-07-31 16:49:24 +0800

    test: add totp tests

--- tests/dotenvx_test.rs
+++ tests/dotenvx_test.rs
@@ -1,4 +1,6 @@
 use std::env;
+use totp_rs::{Algorithm, Secret, TOTP};
+
 #[test]
 fn test_dotenv_load() {
     // Load the .env file
@@ -15,4 +17,25 @@ fn test_dotenv_load_example() {
     // Check if the environment variable is set
     let value = env::var("HELLO").unwrap();
     println!("HELLO={value}");
+}
+
+#[test]
+fn test_totp() {
+    let totp_url = "otpauth://totp/Dotenvx:demo@example.com?secret=VZOQR7AGS6KWMOOKUWFLSTETI74BW4VT&issuer=Dotenvx";
+    let totp = TOTP::from_url(totp_url).unwrap();
+    println!("{}", totp.generate_current().unwrap());
+}
+
+#[test]
+fn test_generate_secret() {
+    let totp = TOTP::new(
+        Algorithm::SHA1,
+        6,
+        1,
+        30,
+        Secret::default().to_bytes().unwrap(),
+        Some("Dotenvx".to_string()),
+        "john@example.com".to_string(),
+    ).unwrap();
+    println!("{}", totp.get_url())
 }
