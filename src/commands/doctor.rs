320189488bbbc29ffe90f8c7f1f089b5d1a370f2
Libing Chen <libing.chen@gmail.com>
2025-08-02 15:33:20 +0800

    app: doctor sub command

---
+++ src/commands/doctor.rs
@@ -0,0 +1,39 @@
+use crate::commands::linter::lint;
+use crate::commands::list_env_files;
+use clap::ArgMatches;
+use dotenvx_rs::common::get_profile_name_from_file;
+use std::env::current_dir;
+
+pub fn doctor_command(_: &ArgMatches) {
+    let current_dir = current_dir().unwrap();
+    let env_files = list_env_files(current_dir, 1, &None);
+    for env_file in env_files {
+        let file_name = env_file.file_name().to_str().unwrap();
+        let file_path = env_file.path();
+        println!("Checking file: {file_name}");
+        let file_content = std::fs::read_to_string(file_path).unwrap();
+        let public_key_line = file_content
+            .lines()
+            .find(|x| x.starts_with("DOTENV_PUBLIC_KEY"));
+        if let Some(public_key_line) = public_key_line {
+            let public_key_found = public_key_line.split('=').nth(1).unwrap_or("").trim();
+            let profile = get_profile_name_from_file(file_name);
+            if let Some(profile_name) = profile {
+                let public_key_name = format!("DOTENV_PUBLIC_KEY_{}", profile_name.to_uppercase());
+                if public_key_found != public_key_name {
+                    eprintln!(
+                        "Warning: The public key in {file_name} does not match the expected format: {public_key_name}"
+                    );
+                }
+            } else if public_key_found != "DOTENV_PUBLIC_KEY" {
+                eprintln!(
+                    "Warning: The public key in {file_name} does not match the expected format: DOTENV_PUBLIC_KEY"
+                );
+            }
+        } else {
+            eprintln!("Warning: No public key found in {file_name}");
+        }
+    }
+    println!("Linter is running diagnostics...");
+    lint().unwrap();
+}
