70be533251e29bb6d302c6225b7945a56775a519
Libing Chen <libing.chen@gmail.com>
2025-08-04 16:15:42 +0800

    test: polishing

--- tests/demo_test.rs
+++ tests/demo_test.rs
@@ -1,4 +1,8 @@
+use csv::WriterBuilder;
+use dotenv_linter::cli::options::CheckOptions;
 use glob::Pattern;
+use std::io;
+use testresult::TestResult;
 
 #[test]
 fn test_glob() {
@@ -19,6 +23,44 @@ fn test_properties() {
         .rsplit('_')
         .next()
         .map(|x| x.to_string());
-    println!("{:?}", profile_name);
+    println!("{profile_name:?}");
+}
 
+#[test]
+fn test_shell_value_quote() {
+    use shlex::try_quote;
+    let shell_variable_value = "some value \n \" ; ' with spaces and $pecial characters";
+    let escaped_value = try_quote(shell_variable_value).unwrap();
+
+    println!("Escaped value: {}", escaped_value);
+}
+
+#[test]
+fn test_csv() -> TestResult {
+    let mut wtr = WriterBuilder::new()
+        .delimiter(b',') // Use semicolon as delimiter
+        .terminator(csv::Terminator::CRLF) // Use CRLF for line endings
+        .from_writer(io::stdout());
+
+    wtr.write_record(&["Name", "Occupation"])?;
+    wtr.write_record(&["Jane Doe", "Engin\"eer"])?;
+    wtr.flush()?;
+    Ok(())
+}
+
+#[test]
+fn test_linter() {
+    use dotenv_linter::{check, cli};
+    // Simulate command-line arguments
+    let args_vec = vec![
+        "dotenv-linter",
+        "--skip",
+        "UnorderedKey",
+        "--exclude",
+        ".env.keys",
+    ];
+    let current_dir = std::env::current_dir().unwrap();
+    let matches = cli::command().try_get_matches_from(args_vec).unwrap();
+    let options = CheckOptions::new(&matches);
+    check(&options, &current_dir).unwrap();
 }
