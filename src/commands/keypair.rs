b0ef62abb0d8c6a3e14985b528152a29422cb798
Libing Chen <libing.chen@gmail.com>
2025-08-12 00:21:23 +0800

    chore: fix key pair find

--- src/commands/keypair.rs
+++ src/commands/keypair.rs
@@ -1,6 +1,6 @@
 use crate::commands::model::KeyPair;
 use crate::commands::{
-    find_all_keys, find_dotenv_keys_file, get_env_file_arg, get_private_key, get_private_key_name,
+    find_all_keys, find_private_key_from_home, get_env_file_arg, get_private_key, get_private_key_name,
     get_public_key, get_public_key_name, write_key_pair, write_private_key_to_file, write_public_key_to_file,
     EcKeyPair, KEYS_FILE_NAME,
 };
@@ -10,7 +10,6 @@ use colored_json::to_colored_json_auto;
 use dotenvx_rs::common::get_profile_name_from_file;
 use prettytable::format::Alignment;
 use prettytable::{row, Cell, Row, Table};
-use std::env;
 
 pub fn keypair_command(command_matches: &ArgMatches, profile: &Option<String>) {
     // import private key
@@ -30,59 +29,52 @@ pub fn keypair_command(command_matches: &ArgMatches, profile: &Option<String>) {
     let profile_name = get_profile_name_from_file(&env_file);
     let env_private_key_name = get_private_key_name(&profile_name);
     let env_pub_key_name = get_public_key_name(&profile_name);
-    let keys_file_path = find_dotenv_keys_file();
-    if env::var(&env_private_key_name).is_err() && keys_file_path.is_none() {
-        if format == "shell" {
-            println!("{env_pub_key_name}=\n{env_private_key_name}=");
-        } else {
-            let body = serde_json::json!({
-                env_pub_key_name: "".to_string(),
-                env_private_key_name: "".to_string(),
-            });
-            println!("{}", to_colored_json_auto(&body).unwrap());
+    let public_key = get_public_key(&profile_name);
+    let mut private_key: Option<String> = None;
+    if let Ok(public_key_hex) = &public_key {
+        private_key = find_private_key_from_home(public_key_hex);
+    }
+    if private_key.is_none() {
+        private_key = get_private_key(&profile_name).ok();
+    }
+    // check key pair validity
+    if let Some(private_key_hex) = &private_key
+        && let Ok(public_key_hex) = &public_key
+    {
+        let kp = EcKeyPair::from_secret_key(private_key_hex);
+        let reversed_pk_hex = kp.get_pk_hex();
+        if &reversed_pk_hex != public_key_hex {
+            eprintln!("{}", "The public key does not match the private key:".red());
+            eprintln!("{env_pub_key_name}={public_key_hex}");
+            eprintln!("{env_private_key_name}={private_key_hex}");
+            std::process::exit(1);
         }
+    }
+    // dump the public key to .env file and private key to .env.keys file
+    if command_matches.get_flag("dump") {
+        let public_key_hex = public_key.unwrap().to_string();
+        let key_pair = KeyPair::new(&public_key_hex, &private_key.unwrap(), profile);
+        write_public_key_to_file(&env_file, &public_key_hex).unwrap();
+        write_private_key_to_file(KEYS_FILE_NAME, &env_private_key_name, &key_pair).unwrap();
+        return;
+    }
+    if format == "shell" {
+        println!(
+            "export {}={}",
+            env_pub_key_name,
+            public_key.unwrap_or_else(|_| "".to_owned())
+        );
+        println!(
+            "export {}={}",
+            env_private_key_name,
+            private_key.unwrap_or("".to_owned())
+        );
     } else {
-        let private_key = get_private_key(&profile_name);
-        let public_key = get_public_key(&profile_name);
-        // check key pair validity
-        if private_key.is_ok() && public_key.is_ok() {
-            let public_key_hex = public_key.as_ref().unwrap();
-            let private_key_hex = private_key.as_ref().unwrap();
-            let kp = EcKeyPair::from_secret_key(private_key_hex);
-            let reversed_pk_hex = kp.get_pk_hex();
-            if &reversed_pk_hex != public_key_hex {
-                eprintln!("{}", "The public key does not match the private key:".red());
-                eprintln!("{env_pub_key_name}={public_key_hex}");
-                eprintln!("{env_private_key_name}={private_key_hex}");
-                std::process::exit(1);
-            }
-        }
-        // dump the public key to .env file and private key to .env.keys file
-        if command_matches.get_flag("dump") {
-            let public_key_hex = public_key.unwrap().to_string();
-            let key_pair = KeyPair::new(&public_key_hex, &private_key.unwrap(), profile);
-            write_public_key_to_file(&env_file, &public_key_hex).unwrap();
-            write_private_key_to_file(KEYS_FILE_NAME, &env_private_key_name, &key_pair).unwrap();
-            return;
-        }
-        if format == "shell" {
-            println!(
-                "export {}={}",
-                env_pub_key_name,
-                public_key.unwrap_or_else(|_| "".to_owned())
-            );
-            println!(
-                "export {}={}",
-                env_private_key_name,
-                private_key.unwrap_or_else(|_| "".to_owned())
-            );
-        } else {
-            let body = serde_json::json!({
-                env_pub_key_name: public_key.unwrap_or_else(|_| "".to_owned()),
-                env_private_key_name: private_key.unwrap_or_else(|_| "".to_owned()),
-            });
-            println!("{}", to_colored_json_auto(&body).unwrap());
-        }
+        let body = serde_json::json!({
+            env_pub_key_name: public_key.unwrap_or_else(|_| "".to_owned()),
+            env_private_key_name: private_key.unwrap_or("".to_owned()),
+        });
+        println!("{}", to_colored_json_auto(&body).unwrap());
     }
 }
 
