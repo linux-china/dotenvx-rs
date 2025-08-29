95d47b9c66ba3c61bc4ca2751af7fc92c82d1d74
Libing Chen <libing.chen@gmail.com>
2025-08-29 14:48:39 +0800

    chore: add uv python support

--- src/commands/link.rs
+++ src/commands/link.rs
@@ -1,6 +1,7 @@
 use clap::ArgMatches;
 use colored::Colorize;
-use std::path::PathBuf;
+use std::fs::Permissions;
+use std::path::{Path, PathBuf};
 use symlink::symlink_file;
 
 pub fn link_command(command_matches: &ArgMatches, dotenvx_name: &str) {
@@ -8,6 +9,10 @@ pub fn link_command(command_matches: &ArgMatches, dotenvx_name: &str) {
         .get_one::<String>("command")
         .map(|s| s.as_str())
         .unwrap();
+    if command_name.ends_with("venv/bin/python") {
+        link_uv_python(command_name);
+        return;
+    }
     let target_path = PathBuf::from(command_name);
     if target_path.exists() {
         eprintln!("The target path already exists: {}", target_path.display());
@@ -28,4 +33,40 @@ pub fn link_command(command_matches: &ArgMatches, dotenvx_name: &str) {
         "{}",
         format!("${command_name} created and linked to dotenvx executable").green()
     );
+}
+
+fn link_uv_python(command_name: &str) {
+    let canonical_path = std::fs::canonicalize(command_name).unwrap();
+    let real_path = canonical_path.to_string_lossy().to_string();
+    std::fs::remove_file(command_name).unwrap();
+    let python3_script = format!(
+        r#"
+#!/bin/bash
+
+# load .env by dotenvx
+eval $( dotenvx decrypt --stdout --format shell )
+# Execute python3 command with arguments
+{real_path} "$@"
+"#
+    );
+    std::fs::write(command_name, python3_script).unwrap();
+    set_executable(command_name);
+    println!(
+        "{command_name} replaced with dotenvx and python3"
+    );
+}
+
+#[cfg(unix)]
+fn set_executable<P: AsRef<Path>>(path: P) {
+    use std::os::unix::fs::PermissionsExt;
+    std::fs::set_permissions(path, Permissions::from_mode(0o755)).unwrap();
+}
+
+#[cfg(not(unix))]
+fn set_executable<P: AsRef<Path>>(path: P) {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_resolve_link() {}
 }
