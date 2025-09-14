8ab667496a1441594160742ef7031431ea33fa5e
Libing Chen <libing.chen@gmail.com>
2025-09-14 14:51:16 +0800

    chore: polishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! # Dotenvx
+//!
+//! A library to load environment variables from `.env` files with encrypted mode, and compatible with dotenv api.
+//! # Features
+//! - Load environment variables from `.env` files.
+//! - Support for decrypting environment variables.
+//! - Support for profile environment variables, such as `NODE_ENV`, `RUN_ENV`, `APP_ENV`, `SPRING_PROFILES_ACTIVE`, `STELA_ENV`.
+//! - Override existing environment variables by default because of security consideration.
+//!
+//! # Examples
+//!
+//! ```
+//! use dotenvx_rs::dotenvx;
+//!
+//! dotenvx::dotenv().ok();
+//! ```
+
 pub mod common;
 pub mod dotenvx;
 
