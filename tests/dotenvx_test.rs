2c8d433fa665c3dd5ba371b5581a102faf3844db
linux_china <libing.chen@gmail.com>
2026-08-17 20:32:47 +0800

    test: update api

--- tests/dotenvx_test.rs
+++ tests/dotenvx_test.rs
@@ -1,5 +1,5 @@
 use std::env;
-use totp_rs::{Algorithm, Secret, TOTP};
+use totp_rs::{Algorithm, Secret, Totp};
 
 #[test]
 fn test_dotenv_load() {
@@ -22,20 +22,20 @@ fn test_dotenv_load_example() {
 #[test]
 fn test_totp() {
     let totp_url = "otpauth://totp/Dotenvx:demo@example.com?secret=VZOQR7AGS6KWMOOKUWFLSTETI74BW4VT&issuer=Dotenvx";
-    let totp = TOTP::from_url(totp_url).unwrap();
-    println!("{}", totp.generate_current().unwrap());
+    let totp = Totp::from_url(totp_url).unwrap();
+    println!("{}", totp.generate_current());
 }
 
 #[test]
 fn test_generate_secret() {
-    let totp = TOTP::new(
-        Algorithm::SHA1,
-        6,
-        1,
-        30,
-        Secret::default().to_bytes().unwrap(),
-        Some("Dotenvx".to_string()),
-        "john@example.com".to_string(),
-    ).unwrap();
-    println!("{}", totp.get_url())
+    let totp = totp_rs::Builder::new()
+        .with_algorithm(Algorithm::SHA1)
+        .with_digits(6)
+        .with_skew(1)
+        .with_step_duration(30)
+        .with_secret(Secret::default().as_bytes())
+        .with_account_name("john@example.com".to_string())
+        .build()
+        .unwrap();
+    println!("{}", totp.to_url().unwrap())
 }
